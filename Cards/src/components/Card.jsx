819614d21b5fd8a7da2a69566a64e1c167d42ed3
Ankit Singh <rixronger@gmail.com>
2024-09-02 23:42:45 +0530

    Add z index to the card

--- Cards/src/components/Card.jsx
+++ Cards/src/components/Card.jsx
@@ -46,7 +46,7 @@ const Card = ({ img, text, state, degree }) => {
 
   return (
     <>
-      <div className={`w-[400px] absolute bg-gray-100 p-5 shadow-md bg-[url(images/paper.png)] flex flex-col justify-center items-center select-none`}
+      <div className={`w-[400px] absolute bg-gray-100 p-5 shadow-md bg-[url(images/paper.png)] flex flex-col justify-center items-center select-none hover:z-50`}
         style={{
           transform: `translate(${position.x}px, ${position.y}px) rotate(${degree}deg)`,
           cursor: dragging ? "grabbing" : "grab",
@@ -56,7 +56,7 @@ const Card = ({ img, text, state, degree }) => {
         onMouseUp={onMouseUp}
       >
         <h1 className="text-[#1D3557] text-4xl text-center">{text}</h1>
-        <div className="group w-full h-[400px] mt-5 border-8 border-red-500 flex justify-center items-center relative">
+        <div className="group w-full h-[400px] mt-5 flex justify-center items-center relative">
           <img src={img} className="w-full h-full" draggable="false" />
           <div className="w-3/4 bg-white px-3 py-2 text-center text-[#1D3557] rounded-sm absolute shadow-2xl -rotate-12 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
             <span className="text-2xl">Sunday, July </span>
